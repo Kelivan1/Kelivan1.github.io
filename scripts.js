66506d369c11143c3e18e699243b82e839f2d175
Kellen Sullivan <156628645+Kelivan1@users.noreply.github.com>
2024-07-31 14:38:37 -0700

    Attempt to have button switch pages

--- scripts.js
+++ scripts.js
@@ -1,15 +1,1 @@
-function jumpToAboutMe() {
-    // Get the header element
-    var header = document.getElementById('About-Me-Header');
-    
-    // Use scrollIntoView to scroll to the header
-    header.scrollIntoView({ behavior: 'smooth' });
-}
-
-function jumpToProjects() {
-    // Get the header element
-    var header = document.getElementById('Project-Header');
-    
-    // Use scrollIntoView to scroll to the header
-    header.scrollIntoView({ behavior: 'smooth' });
-}
+/* None yet :) */
